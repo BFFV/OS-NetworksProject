1e54cafb22c1a58b97a726dc8f5f9e1df4a0887d
msmendoza <msmendoza@uc.cl>
2021-06-20 03:47:39 -0400

    ssh tunnel with libssh.h (further tests remaining)

--- P2/client/src/connection.h
+++ P2/client/src/connection.h
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <stdlib.h>
 #include <netinet/in.h>
 #include <sys/un.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
 #include <errno.h>
+#define LIBSSH_STATIC 1
+#include <libssh/libssh.h>
 #include "communication.h"
 
 
 // Connect to server
 int prepare_socket(char* IP, int PORT);
+
+// Verify known hosts
+int verify_knownhost(ssh_session session);
--- P2/client/src/main.c
+++ P2/client/src/main.c
@@ -1,5 +1,3 @@
-#include <unistd.h>
-#include <stdio.h>
 #include <stdbool.h>
 #include "connection.h"
 #include "communication.h"
@@ -23,6 +21,52 @@ char* get_input() {
 // Monster Hunter Client
 int main (int argc, char *argv[]) {
 
+    // https://api.libssh.org/stable/libssh_tutor_guided_tour.html
+    // https://api.libssh.org/stable/libssh_tutor_forwarding.html (se debe implementar reverse port forwarding) según enunciado
+    // TODO: eliminar el undefined symbols for arch x86_64 (error)
+    //          Posible workaround si se usa libssh2.h
+    ssh_session client_ssh_session = ssh_new();
+    int rc;
+    char *password;
+    int verbosity = SSH_LOG_PROTOCOL;
+    int port = 22;
+    if (client_ssh_session == NULL) {
+        exit(-1);
+    }
+
+    // SSH options
+    ssh_options_set(client_ssh_session, SSH_OPTIONS_HOST, "iic2333.ing.puc.cl");
+    ssh_options_set(client_ssh_session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
+    ssh_options_set(client_ssh_session, SSH_OPTIONS_PORT, &port);
+
+    // 1, Connect to SSH server
+    rc = ssh_connect(client_ssh_session);
+    if (rc != SSH_OK) {
+        fprintf(stderr, "Error connecting to iic2333.ing.puc.cl: %s\n",
+                ssh_get_error(client_ssh_session));
+        exit(-1);
+    }
+
+    // 2. Verify the server's identity
+    // For the source code of verify_knownhost(), check previous example
+    if (verify_knownhost(client_ssh_session) < 0) {
+        ssh_disconnect(client_ssh_session);
+        ssh_free(client_ssh_session);
+        exit(-1);
+    }
+
+    // 3. Authenticate ourselves
+    rc = ssh_userauth_none(client_ssh_session, NULL);  // no password is asked
+    if (rc != SSH_AUTH_SUCCESS) {
+        fprintf(stderr, "Error authenticating with password: %s\n",
+                ssh_get_error(client_ssh_session));
+        ssh_disconnect(client_ssh_session);
+        ssh_free(client_ssh_session);
+        exit(-1);
+    }
+
+    // -------------------------------------------------------------------
+
     // Initialize client
     int client = prepare_socket(argv[2], atoi(argv[4]));
 
@@ -45,6 +89,12 @@ int main (int argc, char *argv[]) {
 
     // Close client socket
     close(client);
+
+    // -------------------------------------------------------------------
+
+    // 5. Close the ssh session
+    ssh_disconnect(client_ssh_session);
+    ssh_free(client_ssh_session);
 
     return 0;
 }
